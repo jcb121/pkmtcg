68ba3755664271a49cee4b5a132bf6934c6227b2
jcb121 <jcb.121@hotmail.co.uk>
2016-05-25 19:14:03 +0100

    added nav for sidenav

---
+++ src/client/app/components/pokemonDecksSide/pokemonDecksSide.js
@@ -0,0 +1,59 @@
+rootApp.directive("pokemonDecksSide", function () {
+    return {
+        templateUrl: "components/pokemonDecksSide/pokemonDecksSide.html",
+        scope: {},
+        controller: function ($scope, decks, users) {
+			$scope.Decks = decks;
+			$scope.Users = users;
+
+			$scope.addCard = function(id){
+				var index = this.indexOfCard(id);
+				if (index !== false) {
+					this.cards[index].quantity++;
+				}
+				else {
+					this.cards.push({ id: id, quantity: 1 });
+				}
+				return this;
+			};
+
+			$scope.removeCard = function(id){
+				var index = this.indexOfCard(id);
+				if (this.cards[index].quantity <= 1) {
+					this.deleteCard(id);
+				}
+				else {
+					this.cards[index].quantity--;
+				}
+			};
+
+			$scope.deleteCard = function(id){
+				var index = this.indexOfCard(id);
+				if(index){
+					this.cards.splice(index, 1);
+				}
+			};
+
+			$scope.indexOfCard = function(id){
+				var deepCheck = false;
+				this.cards.forEach(function (deckCard, index) {
+					if (deckCard.id == id) deepCheck = index;
+				});
+				return deepCheck;
+			};
+
+			$scope.quantityOfCard = function (id){
+				var index = this.indexOfCard(id);
+				return this.cards[index].quantity;
+			};
+
+			$scope.totalCards = function(){
+				var cardCount = 0;
+				this.cards.forEach(function (card) {
+				 cardCount += card.quantity;
+				});
+				return cardCount;
+			};
+        }
+    };
+});

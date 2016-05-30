184baedaaaaf0bf07fc23bb0f2d7abb0c847d3ef
jcb121 <jcb.121@hotmail.co.uk>
2016-05-30 14:50:16 +0100

    removed pokemoncards directive

--- src/client/app/features/home/home.js
+++ src/client/app/features/home/home.js
@@ -1,3 +1,19 @@
-rootApp.controller("home", function ($scope, $mdSidenav ) {
+rootApp.controller("home", function ($scope, cards ) {
+  $scope.filters = {};
+  $scope.filters.pageNo = 0;
+  $scope.filters.perPage = 14;
 
+  $scope.totalCards = 0;
+  $scope.cards = [];
+  $scope.pageCount = 0;
+
+  $scope.$watch( function(){ return $scope.filters; }, function(filters){
+    cards.get(filters).then(setCards);
+  }, true);
+
+  function setCards(cards){
+    $scope.cards = cards.cards;
+    $scope.totalCards = cards.results;
+    $scope.pageCount = Math.ceil( cards.results / $scope.filters.perPage);
+  }
 });
